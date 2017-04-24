0e8ccc3a2653a4bf269c955e4af167f6430aaf44
Stefan Jung <stefan@theappbusiness.com>
2017-04-24 10:27:48 +0200

    .

--- HelloWorld/app.js
+++
@@ -1,4 +0,0 @@
-function handleClick() {
-  var hello_world = document.getElementById("hello_world");
-  hello_world.innerHTML = "Hello Berlin!";
-}
---
+++ HelloWorld/test/button.test.js
@@ -0,0 +1,4 @@
+function test_button() {
+  var node = this.refs.button;
+  ReactTestUtils.Simulate.click(node);
+}
